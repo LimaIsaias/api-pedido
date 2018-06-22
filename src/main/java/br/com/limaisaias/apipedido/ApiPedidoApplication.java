035baf663a5b081fa81335dbf2fb77f000d4d4e6
LimaIsaias <coelholimaisaias@gmail.com>
2018-06-21 22:55:29 -0300

    'Genesis'

---
+++ src/main/java/br/com/limaisaias/apipedido/ApiPedidoApplication.java
@@ -0,0 +1,12 @@
+package br.com.limaisaias.apipedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPedidoApplication.class, args);
+	}
+}
---
+++ src/main/java/br/com/limaisaias/apipedido/resources/CategoriaResource.java
@@ -0,0 +1,16 @@
+package br.com.limaisaias.apipedido.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value ="/categorias")
+public class CategoriaResource {
+	
+	@GetMapping
+	public String listar() {
+		return "Funcionou";
+	}
+
+}

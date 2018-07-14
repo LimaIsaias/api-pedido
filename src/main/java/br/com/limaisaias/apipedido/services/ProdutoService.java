26cb2d1e625117ca41e40c70399c2e39105ca7c9
LimaIsaias <coelholimaisaias@gmail.com>
2018-07-14 15:08:40 -0300

    service e repository de produto

---
+++ src/main/java/br/com/limaisaias/apipedido/services/ProdutoService.java
@@ -0,0 +1,29 @@
+package br.com.limaisaias.apipedido.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.limaisaias.apipedido.domains.Categoria;
+import br.com.limaisaias.apipedido.domains.Produto;
+import br.com.limaisaias.apipedido.repository.ProdutoRepository;
+import br.com.limaisaias.apipedido.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository repository;
+	
+	public List<Produto> findAll() {
+		return repository.findAll();
+	}
+
+	public Produto findByID(Integer id) throws ObjectNotFoundException {
+		Optional<Produto> retorno = repository.findById(id);
+		return retorno.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado ! Id:"+ id +" Tipo:"+Produto.class.getName()));
+	}
+	
+}

8529792b786cb8bc0facbac509e589a1b0a0d4ec
LimaIsaias <coelholimaisaias@gmail.com>
2018-07-23 21:30:25 -0300

    classe estado

--- src/main/java/br/com/limaisaias/apipedido/domains/Estado.java
+++ src/main/java/br/com/limaisaias/apipedido/domains/Estado.java
@@ -11,7 +11,6 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;

24f6d6ed6f679967b7f17d6f07a39f471fd4512e
LimaIsaias <coelholimaisaias@gmail.com>
2018-08-04 13:46:38 -0300

    Cliente GET/POST/PUT/DELETE | ValidationError | FieldMessage

--- src/main/java/br/com/limaisaias/apipedido/resources/ClienteResource.java
+++ src/main/java/br/com/limaisaias/apipedido/resources/ClienteResource.java
@@ -1,15 +1,24 @@
 package br.com.limaisaias.apipedido.resources;
 
+import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.limaisaias.apipedido.domains.Cliente;
+import br.com.limaisaias.apipedido.dto.ClienteDTO;
 import br.com.limaisaias.apipedido.services.ClienteService;
 
 @RestController
@@ -19,10 +28,11 @@ public class ClienteResource {
 	@Autowired
 	private ClienteService service;
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ResponseEntity<List<Cliente>> findAll() {
-		List<Cliente> retorno = service.findAll();
-		return ResponseEntity.ok().body(retorno);
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		List<Cliente> list = service.findAll();
+		List<ClienteDTO> listDto = list.stream().map(ClienteDTO::new).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
@@ -32,4 +42,35 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(retorno);
 	}
 
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody ClienteDTO objDto, @PathVariable Integer id) {
+		Cliente obj = service.fromDTO(objDto);
+		obj.setId(id);
+		service.update(obj);
+		return ResponseEntity.ok().build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDto = list.map(ClienteDTO::new);
+		return ResponseEntity.ok().body(listDto);
+	}
 }
--- src/main/java/br/com/limaisaias/apipedido/services/ClienteService.java
+++ src/main/java/br/com/limaisaias/apipedido/services/ClienteService.java
@@ -4,10 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.limaisaias.apipedido.domains.Cliente;
+import br.com.limaisaias.apipedido.dto.ClienteDTO;
 import br.com.limaisaias.apipedido.repository.ClienteRepository;
+import br.com.limaisaias.apipedido.services.exceptions.DataIntegrityException;
 import br.com.limaisaias.apipedido.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -15,14 +21,48 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repository;
-	
 
 	public List<Cliente> findAll() {
 		return repository.findAll();
 	}
 
-	public Cliente find(Integer id) {
+	public Cliente find(Integer id) throws ObjectNotFoundException {
 		Optional<Cliente> retorno = repository.findById(id);
-		return retorno.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado ! Id:"+ id +" Tipo:"+Cliente.class.getName()));
+		return retorno.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado ! Id:" + id + " Tipo:" + Cliente.class.getName()));
+	}
+
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente objNew = find(obj.getId());
+		updateData(objNew, obj);
+		return repository.save(objNew);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+		}
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO clienteDTO) {
+		return new Cliente(clienteDTO.getId(), clienteDTO.getNome(), clienteDTO.getEmail(), null, null);
+	}
+
+	private void updateData(Cliente objNew, Cliente obj) {
+		objNew.setNome(obj.getNome());
+		objNew.setEmail(obj.getEmail());
 	}
 }
